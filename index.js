f1bdfdf77c338163b363e96cad0439923e06302d
Noctea <38964329+Noctea@users.noreply.github.com>
2018-11-30 00:47:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ var prefix = (".");
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "🆕bienvenue🆕").send(`:point_right: Bienvenue, ${member} sur le Discord de **SlowelyGames** :tada::hugging: !`)
 })
+
+
+bot.on('message', message => {
+    if(message.content === prefix + "staff") {
+        message.channel.send("<@&468174150394183690>")
+        console.log(`${message.author.username} contact le Staff!`);
+        let serverembed = new Discord.RichEmbed()
+        .setTitle("Demande de STAFF")
+        .setDescription("Un membre du <@&468174150394183690> te contactera dans quelque instants.")
+        message.react("✅")
+        message.channel.send(serverembed);
+    }
+})
