136257450e021e03667a77070efefe601a9c93d7
Noctea <38964329+Noctea@users.noreply.github.com>
2018-11-30 00:25:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ bot.on("ready", function () {
     bot.user.setActivity(".help | SlowelyBot V2");
 });
 
-bot.login(process.env.TOKEN));
+bot.login(process.env.TOKEN);
 
 var prefix = (".");
 
+
 bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "🆕bienvenue🆕").send(":point_right: Bienvenue ${member} sur le Discord de **Slowely Games** :tada::hugging: !")
+    member.guild.channels.find("name", "🆕bienvenue🆕").send(`:point_right: Bienvenue, ${member} sur le Discord de **SlowelyGames** :tada::hugging: !`)
 })
