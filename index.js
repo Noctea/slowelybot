9f07110260fa3f7d472019b26bc4b6d57ddfc205
Noctea <38964329+Noctea@users.noreply.github.com>
2018-11-30 00:54:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function () {
-    bot.user.setActivity(".help | SlowelyBot V2");
+    bot.user.setActivity("SlowelyBot V2 | EN DEV");
 });
 
 bot.login(process.env.TOKEN);
