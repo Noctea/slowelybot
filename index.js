91315f220adc5e1aa925258a6004e2408b493e6c
Noctea <nocteagaming@gmail.com>
2018-11-30 03:15:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ bot.on("message", message => {
             .setDescription("Commandes Disponible avec <@517662031026061314>")
             .addField(".help", "Affiche cette page")
             .addField(".staff", "Mentionne le Staff (ne pas spam sous peine de bannissement)")
+            .addField(".infos", "Affiche les informations du serveur Discord")
             .setThumbnail("http://portaildoc-veto.vetagro-sup.fr/wp-content/uploads/2018/05/brain.png")
             .setColor("0xFF9933")
             .setFooter("Le BOT est encore en développement ; d'autres commandes seront bientôt disponible.")
