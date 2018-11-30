0798a4ac54ecf3d1daa3b6f58870e9be085171c7
Noctea <38964329+Noctea@users.noreply.github.com>
2018-11-30 01:16:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,25 @@ bot.on("guildMemberAdd", member => {
 })
 
 
-bot.on('message', message => {
+bot.on("message", message => {
+    if (message.content === prefix + "help") {
+        var embed = new Discord.RichEmbed()
+            .setTitle("Commandes :")
+            .setDescription("Commandes Disponible avec <@517662031026061314>")
+            .addField(".help", "Affiche cette page")
+            .addField(".staff", "Mentionne le Staff (ne pas spam sous peine de bannissement)")
+            .setThumbnail("http://portaildoc-veto.vetagro-sup.fr/wp-content/uploads/2018/05/brain.png")
+            .setColor("0xFF9933")
+            .setFooter("Le BOT est encore en développement, d'autres commandes seront bientôt disponible.")
+        message.channel.sendEmbed(embed);
+    }
+
     if(message.content === prefix + "staff") {
         message.channel.send("<@&468174150394183690>")
-        console.log(`${message.author.username} contact le Staff!`);
-        let serverembed = new Discord.RichEmbed()
-        .setTitle("Demande de STAFF")
-        .setDescription("Un membre du <@&468174150394183690> te contactera dès que possible.")
+        var embed = new Discord.RichEmbed()
+            .setTitle("Demande de STAFF")
+            .setDescription("Un membre du <@&468174150394183690> te contactera dès que possible.")
         message.react("✅")
-        message.channel.send(serverembed);
+        message.channel.sendEmbed(embed);
     }
 })
