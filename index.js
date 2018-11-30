5b19189889bc78c32b49d0d9fb2d59074d6e3138
Noctea <38964329+Noctea@users.noreply.github.com>
2018-11-30 01:28:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on("message", message => {
             .addField(".staff", "Mentionne le Staff (ne pas spam sous peine de bannissement)")
             .setThumbnail("http://portaildoc-veto.vetagro-sup.fr/wp-content/uploads/2018/05/brain.png")
             .setColor("0xFF9933")
-            .setFooter("Le BOT est encore en développement, d'autres commandes seront bientôt disponible.")
+            .setFooter("Le BOT est encore en développement ; d'autres commandes seront bientôt disponible.")
         message.channel.sendEmbed(embed);
     }
 
