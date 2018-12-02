2a2d9bacc211fbbea7b78ca85f5d07bd7b0a69c6
Noctea <nocteagaming@gmail.com>
2018-12-03 00:25:55 +0100

    Update infos.js

--- command/infos.js
+++ command/infos.js
@@ -7,17 +7,22 @@ module.exports = class Infos extends Command {
 
     static match(message) {
         if (message.content === prefix + "infos") {
-            var infos = new Discord.RichEmbed()
-                .setDescription("Informations sur le Discord :")
-                .addField("Nom du Discord", message.guild.name)
-                .addField("Crée le", "Mardi 10 Juillet 2018 à 15h38 (Heure de Paris)")
-                .addField("Tu as rejoint le", message.member.joinedAt)
-                .addField("Nombres de membres sur le Discord", message.guild.memberCount)
-                .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Info_icon_002.svg/768px-Info_icon_002.svg.png")
-                .setColor("0x3333FF")
-            message.react("👀")
-            message.channel.sendEmbed(infos)
             return true
         }
+    }
+
+    static action(message) {
+
+        var infos = new Discord.RichEmbed()
+            .setDescription("Informations sur le Discord :")
+            .addField("Nom du Discord", message.guild.name)
+            .addField("Crée le", "Mardi 10 Juillet 2018 à 15h38 (Heure de Paris)")
+            .addField("Tu as rejoint le", message.member.joinedAt)
+            .addField("Nombres de membres sur le Discord", message.guild.memberCount)
+            .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Info_icon_002.svg/768px-Info_icon_002.svg.png")
+            .setColor("0x3333FF")
+
+        message.channel.sendEmbed(infos)
+        message.react("👀")
     }
 }
