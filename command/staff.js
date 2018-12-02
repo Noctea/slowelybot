9c5af1cbceed951fab580499189d5ac37679a200
Noctea <nocteagaming@gmail.com>
2018-12-03 00:44:35 +0100

    Update staff.js

--- command/staff.js
+++ command/staff.js
@@ -20,7 +20,7 @@ module.exports = class Staff extends Command {
             .setDescription("Un membre du <@&502113512756084736> te contactera dès que possible.")
             .setThumbnail("https://www.nativitychurch.org/wp-content/uploads/2018/02/staff-box-1280x450.jpg")
             .setColor("0xBF00FF")
-            .setFooter(`Demande de ${message.author.username}`)
+            .setFooter(`Demande de **${message.author.username}**`)
 
         message.channel.sendEmbed(staff)
         .then(function(message) {
