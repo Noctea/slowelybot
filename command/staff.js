f7e4ab32331903fbf5d16fcb0bb4fc7be9ad69bd
Noctea <nocteagaming@gmail.com>
2018-12-03 00:25:58 +0100

    Update staff.js

--- command/staff.js
+++ command/staff.js
@@ -7,16 +7,22 @@ module.exports = class Staff extends Command {
 
     static match(message) {
         if (message.content === prefix + "staff") {
-            message.channel.send("<@&502113512756084736>")
-            var staff = new Discord.RichEmbed()
-                .setTitle("Demande de STAFF :")
-                .setDescription("Un membre du <@&502113512756084736> te contactera dès que possible.")
-                .setThumbnail("https://www.nativitychurch.org/wp-content/uploads/2018/02/staff-box-1280x450.jpg")
-                .setColor("0xBF00FF")
-                .setFooter("Patiente un peu...")
-            message.react("✅")
-            message.channel.sendEmbed(staff)
             return true
         }
+    }
+
+    static action(message) {
+
+        message.channel.send("<@&502113512756084736>")
+
+        var staff = new Discord.RichEmbed()
+            .setTitle("Demande de STAFF :")
+            .setDescription("Un membre du <@&502113512756084736> te contactera dès que possible.")
+            .setThumbnail("https://www.nativitychurch.org/wp-content/uploads/2018/02/staff-box-1280x450.jpg")
+            .setColor("0xBF00FF")
+            .setFooter(`Demande de ${message.author.username}`)
+
+        message.channel.sendEmbed(staff)
+        message.react("✅")
     }
 }
