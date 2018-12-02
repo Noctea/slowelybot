9cc62bafe1b0f6dcf75fde8bf746eaed223f26dd
Noctea <nocteagaming@gmail.com>
2018-12-02 07:53:52 +0100

    Create help.js

---
+++ command/help.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const Command = require("./command")
+
+const prefix = (".")
+
+module.exports = class Help extends Command {
+
+    static match(message) {
+        if (message.content === prefix + "help") {
+            const embed = new Discord.RichEmbed()
+                .setTitle("Commandes :")
+                .setDescription("Commandes Disponible avec <@517662031026061314>")
+                .addField(".help", "Affiche cette page")
+                .addField(".staff", "Mentionne le Staff (ne pas spam sous peine de bannissement)")
+                .addField(".infos", "Affiche les informations du serveur Discord")
+                .setThumbnail("http://portaildoc-veto.vetagro-sup.fr/wp-content/uploads/2018/05/brain.png")
+                .setColor("0xFF9933")
+                .setFooter("Le BOT est encore en développement ; d'autres commandes seront bientôt disponible.")
+            message.react("👌")
+            message.channel.sendEmbed(embed)
+            return true
+        }
+    }
+}
