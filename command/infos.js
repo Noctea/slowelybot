32cf2dd3b70d70b6f87ae33a5aed63b977d7f9bf
Noctea <nocteagaming@gmail.com>
2018-12-02 07:27:08 +0100

    Update infos.js

---
+++ command/infos.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+const Command = require("./command")
+
+const prefix = (".")
+
+module.exports = class Infos extends Command {
+
+    static match(message) {
+        if(message.content === prefix + "infos") {
+            const embed = new Discord.RichEmbed()
+            .setDescription("Informations sur le Discord :")
+            .addField("Nom du Discord", message.guild.name)
+            .addField("Crée le", "Mardi 10 Juillet 2018 à 15h38 (Heure de Paris)")
+            .addField("Tu as rejoint le", message.member.joinedAt)
+            .addField("Nombres de membres sur le Discord", message.guild.memberCount)
+            .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Info_icon_002.svg/768px-Info_icon_002.svg.png")
+            .setColor("0x3333FF")
+        message.react("👀")
+        message.channel.sendEmbed(embed)
+            return true
+        }
+    }
+
+    static action(message) {
+        message.channel.send("pong")
+    }
+}
