33a4fc4b6e9cfa268985345f106a9f8638c085a1
Noctea <nocteagaming@gmail.com>
2018-12-02 07:15:48 +0100

    Create ping.js

---
+++ command/ping.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const Command = require("./command")
+
+module.exports = class Ping extends Command {
+
+    static match(message) {
+        if(message.content === prefix + "ping") {
+            return true
+        }
+    }
+
+    static action(message) {
+        message.channel.send("pong")
+    }
+}
