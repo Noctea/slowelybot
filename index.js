2494aad1000d92d490c06ce9bfae8af15a27a33f
Noctea <nocteagaming@gmail.com>
2018-12-02 06:31:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,24 +52,5 @@ bot.on("message", message => {
         message.react("👀")
         message.channel.sendEmbed(embed)
     }
-
-    if (message.content.startsWith(prefix + "sondage")) {
-        if(message.author.is == "176740082340986880") {
-            let args = message.content.split(" ").slice(1);
-            let thingToEcho = args.join(" ")
-            var embed = new Discord.RichEmbed()
-                .setDescription("Sondage")
-                .addField(thingToEcho, "Répondre avec ✅ ou ❌")
-                .setColor("Ox009933")
-                .setTimestamp()
-            message.guild.channels.find("name", "👌sondage👌").sendEmbed(embed)
-                .then(function (message) {
-                    message.react("✅")
-                    message.react("❌")
-                }).catch(function () {});
-        } else {
-            return message.reply("Tu n'as pas la permission :x:")
-
-        }
-    }
+    
 })
