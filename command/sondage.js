1a95c114626e31a648552e281d4a9027dc394847
Noctea <noctea.pro@gmail.com>
2020-06-03 23:40:07 +0200

    Update sondage.js

--- command/sondage.js
+++ command/sondage.js
@@ -1,32 +1,32 @@
-const Discord = require('discord.js')
-const Command = require('./command')
+const Discord = require("discord.js")
+const Command = require("./command")
 
-const prefix = ('.')
+const prefix = (".")
 
 module.exports = class Sondage extends Command {
 
     static match(message) {
-        if (message.content.startsWith(prefix + 'sondage')) {
+        if (message.content.startsWith(prefix + "sondage")) {
             return true
         }
     }
 
     static action(message) {
 
-        let msg = message.content.split(' ')
+        let msg = message.content.split(" ")
         msg.shift()
-        let question = msg.join(' ')
+        let question = msg.join(" ")
 
         var sondage = new Discord.RichEmbed()
-            .setTitle('Sondage :')
-            .addField(question, '✅ pour oui | ❌ pour non')
-            .setColor('0xFFFFFF')
+            .setTitle("Sondage :")
+            .addField(question, "✅ pour oui | ❌ pour non")
+            .setColor("0xFFFFFF")
             .setFooter(`Créer par ${message.author.username}`)
 
         message.channel.send(sondage)
         .then(function(message) {
-            message.react('✅')
-            message.react('❌')
+            message.react("✅")
+            message.react("❌")
         })
 
         message.delete()
