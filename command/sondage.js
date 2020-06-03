3e3009a9b49687868be85dc696b0eced34670aa9
Noctea <noctea.pro@gmail.com>
2020-06-03 22:05:45 +0200

    Update sondage.js

--- command/sondage.js
+++ command/sondage.js
@@ -6,21 +6,20 @@ const prefix = (".")
 module.exports = class Sondage extends Command {
 
     static match(message) {
-        if (message.content.startWith(prefix + "sondage")) {
+        if (message.content.startsWith(prefix + "sondage")) {
             return true
         }
     }
 
     static action(message) {
 
-        let msg = message.content.split(" ")
+        let msg = message.content.split(' ')
         msg.shift()
-        let question = msg.join(" ")
+        let question = msg.join(' ')
 
         var sondage = new Discord.RichEmbed()
             .setTitle("Sondage :")
-            .setDescription(question, "?")
-            .addField("✅ pour oui | ❌ pour non")
+            .addField(question, "✅ pour oui | ❌ pour non")
             .setThumbnail("https://alapoigneedemains.com/wp-content/uploads/2018/04/104233021.jpg")
             .setColor("0xFFFFFF")
             .setFooter(`Créer par ${message.author.username}`)
